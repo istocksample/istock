696dd0a5038320f35c5c14fc55eb982c993c5872
watertechplumbing <watertechplumbing2015@gmail.com>
2015-05-18 16:54:43 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Istock::Application.config.secret_key_base = 'fdf26764c3fc4b7fd67d9b8d2bed23040e08a625b51c7172d4e7de8d102b6e505776f686b49101002798f6e9952c9dcd74d088346d1c28fffb79f522ba2bdc9d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Istock::Application.config.session_store :cookie_store, key: '_istock_session'
