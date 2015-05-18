48cc092c88f0a6f6dd7d83db7eb80135e92a613a
watertechplumbing <watertechplumbing2015@gmail.com>
2015-05-18 17:41:25 +0530

    index page and css added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Istock::Application.routes.draw do
+
+  root 'static_pages#index'
+
+  get "static_pages/index"
+  get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
